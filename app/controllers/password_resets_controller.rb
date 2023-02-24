4ea5d3d4eb736da9eca86c6c06dcdfd0f4050c9b
mituya <atiutgh@gmail.com>
2023-02-24 20:43:43 +0900

    add:view作成

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,15 +5,7 @@ class PasswordResetsController < ApplicationController
   skip_before_action :require_login
 
   # パスワードリセット申請画面へレンダリングするアクション
-  def new
-  end
-
-  # パスワードのリセットフォーム画面へ遷移するアクション
-  def edit
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(params[:id])
-    　　return not_authenticated if @user.blank?
-  end
+  def new; end
 
   # パスワードのリセットを要求するアクション。
   # ユーザーがパスワードのリセットフォームにメールアドレスを入力して送信すると、このアクションが実行される。
@@ -27,6 +19,13 @@ class PasswordResetsController < ApplicationController
     # 電子メールが見つかったかどうかに関係なく、ユーザーの指示が送信されたことをユーザーに伝えます。
     # これは、システムに存在する電子メールに関する情報を攻撃者に漏らさないためです。
     redirect_to login_path
+  end
+
+  # パスワードのリセットフォーム画面へ遷移するアクション
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+　　return not_authenticated if @user.blank?
   end
 
   # ユーザーがパスワードのリセットフォームを送信したときに発生
@@ -48,32 +47,32 @@ class PasswordResetsController < ApplicationController
   private
 
   def deliver_reset_password_instructions!
-    mail = false
-    config = sorcery_config
-    # hammering protection
-    if config.reset_password_time_between_emails.present? && send(config.reset_password_email_sent_at_attribute_name) && send(config.reset_password_email_sent_at_attribute_name) > config.reset_password_time_between_emails.seconds.ago.utc
-      return false
-    end
+  mail = false
+  config = sorcery_config
+  # hammering protection
+  return false if config.reset_password_time_between_emails.present? && send(config.reset_password_email_sent_at_attribute_name) && send(config.reset_password_email_sent_at_attribute_name) > config.reset_password_time_between_emails.seconds.ago.utc
 
-    self.class.sorcery_adapter.transaction do
-      generate_reset_password_token!
-      mail = send_reset_password_email! unless config.reset_password_mailer_disabled
-    end
-    mail
+  self.class.sorcery_adapter.transaction do
+    generate_reset_password_token!
+    mail = send_reset_password_email! unless config.reset_password_mailer_disabled
+  end
+  mail
   end
 
-  def load_from_reset_password_token(token, &)
-    load_from_token(
-      token,
-      @sorcery_config.reset_password_token_attribute_name,
-      @sorcery_config.reset_password_token_expires_at_attribute_name,
-      &
-    )
+  def load_from_reset_password_token(token, &block)
+  load_from_token(
+    token,
+    @sorcery_config.reset_password_token_attribute_name,
+    @sorcery_config.reset_password_token_expires_at_attribute_name,
+    &block
+  )
   end
 
   def change_password(new_password, raise_on_failure: false)
-    clear_reset_password_token
-    send(:"#{sorcery_config.password_attribute_name}=", new_password)
-    sorcery_adapter.save raise_on_failure:
+  clear_reset_password_token
+  send(:"#{sorcery_config.password_attribute_name}=", new_password)
+  sorcery_adapter.save raise_on_failure: raise_on_failure
   end
+
+
 end
